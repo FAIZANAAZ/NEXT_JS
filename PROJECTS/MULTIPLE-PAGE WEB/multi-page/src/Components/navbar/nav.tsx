2fce97d5d830d8e121cb927247c1d6cb0e1fe829
FAIZANAAZ779 <faixanaaz@gmail.com>
2024-11-06 03:24:26 +0500

    update web

--- PROJECTS/MULTIPLE-PAGE WEB/multi-page/src/Components/navbar/nav.tsx
+++ PROJECTS/MULTIPLE-PAGE WEB/multi-page/src/Components/navbar/nav.tsx
@@ -1,28 +1,158 @@
-import React from 'react'
-import Image from 'next/image'
+"use client";
+import { AnimatePresence, motion } from "framer-motion";
+import { ImMenu } from "react-icons/im";
+import React, { useState } from "react";
+import Image from "next/image";
 
 const Nav = () => {
-  return (
-    <nav className="absolute top-0 right-[10px] w-full flex justify-between items-center p-4 ">
-      <div className="flex items-center ml-[20px]">
+  const [menu, setMenu] = useState(false);
 
-        <Image
-          src={"/giftlog-removebg-preview.png"}
-          width={200}
-          height={200}
-          alt="Gift logo"
-          className="sm:w-[100px] sm:h-[100px] h-[50px] w-[50px]"
-        />
-      </div>
-      <div className="sm:flex lg:space-x-[80px] text-black lg:font-semibold lg:text-[25px] md:space-x-[25px] md:text-[25px]  md:font-[500] sm:space-x-[25px] sm:text-[25px]   sm:font-[500] hidden">
-        <a href="#" className="hover:underline  hover:decoration-orange-400 hover:font-bold transition-all duration-300">Home</a>
-        <a href="#" className="hover:underline  hover:decoration-orange-400 hover:font-bold transition-all duration-300">About</a>
-        <a href="#" className="hover:underline  hover:decoration-orange-400 hover:font-bold transition-all duration-300">Our gifts</a>
-        <a href="#" className="hover:underline  hover:decoration-orange-400 hover:font-bold transition-all duration-300">Shop</a>
-        <a href="#" className="hover:underline  hover:decoration-orange-400 hover:font-bold transition-all duration-300">Contact Us</a>
-      </div>
-    </nav>
-  )
-}
+  let openmenu = () => {
+    setMenu(!menu);
+  };
+
+  return (
+    <>
+      <nav className="absolute top-0  w-full flex justify-between items-center p-4 ">
+        <div className="flex items-center ml-[20px]">
+          <Image
+            src={"/giftlog-removebg-preview.png"}
+            width={200}
+            height={200}
+            alt="Gift logo"
+            className="sm:w-[100px] sm:h-[100px] h-[50px] w-[50px]"
+          />
+        </div>
+        <div className="sm:flex lg:space-x-[80px] text-black lg:font-semibold lg:text-[25px] md:space-x-[30px] md:text-[20px] md:font-[600] sm:space-x-[30px] sm:text-[20px] sm:font-[600] absolute right-[50px] hidden">
+          <a
+            href="#"
+            className="hover:underline hover:decoration-orange-400 hover:font-bold transition-all duration-300"
+          >
+            Home
+          </a>
+          <a
+            href="#"
+            className="hover:underline hover:decoration-orange-400 hover:font-bold transition-all duration-300"
+          >
+            About
+          </a>
+          <a
+            href="#"
+            className="hover:underline hover:decoration-orange-400 hover:font-bold transition-all duration-300"
+          >
+            Our gifts
+          </a>
+          <a
+            href="#"
+            className="hover:underline hover:decoration-orange-400 hover:font-bold transition-all duration-300"
+          >
+            Shop
+          </a>
+          <a
+            href="#"
+            className="hover:underline hover:decoration-orange-400 hover:font-bold transition-all duration-300"
+          >
+            Contact Us
+          </a>
+        </div>
+
+        <div className="flex flex-col justify-center items-end">
+          <button
+            onClick={() => {
+              openmenu();
+            }}
+            className="sm:hidden block"
+          >
+            <ImMenu size={30} color="#000" />
+          </button>
+        </div>
+      </nav>
+
+      <AnimatePresence>
+        {/* for closing */}
+        {menu == true ? (
+          <motion.div
+            className={
+              "absolute top-[60px] right-[2px] flex flex-col space-y-[20px] text-center font-bold  h-[240px] w-[160rpx] bg-black text-white text-[30rpx]  sm:hidden p-4 "
+            }
+            key={"menuu"}
+            initial={{ height: 0, opacity: 0 }}
+            animate={{ height: "240px", opacity: 0.8 }}
+            exit={{ height: 0, opacity: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            <a
+              href="#"
+              className=" hover:underline hover:decoration-orange-400 hover:font-bold transition-all duration-300"
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              className="hover:underline hover:decoration-orange-400 hover:font-bold transition-all duration-300"
+            >
+              About
+            </a>
+            <a
+              href="#"
+              className="hover:underline hover:decoration-orange-400 hover:font-bold transition-all duration-300"
+            >
+              Our gifts
+            </a>
+            <a
+              href="#"
+              className="hover:underline hover:decoration-orange-400 hover:font-bold transition-all duration-300"
+            >
+              Shop
+            </a>
+            <a
+              href="#"
+              className="hover:underline hover:decoration-orange-400 hover:font-bold transition-all duration-300"
+            >
+              Contact Us
+            </a>
+          </motion.div>
+        ) : (
+          <div
+            className={
+              "hidden absolute top-[50px] right-[0px] flex flex-col space-y-[10px] text-center font-bold  h-[250px] w-[120px] bg-white text-[22px]  sm:hidden "
+            }
+          >
+            <a
+              href="#"
+              className="hover:underline hover:decoration-orange-400 hover:font-bold transition-all duration-300"
+            >
+              Home
+            </a>
+            <a
+              href="#"
+              className="hover:underline hover:decoration-orange-400 hover:font-bold transition-all duration-300"
+            >
+              About
+            </a>
+            <a
+              href="#"
+              className="hover:underline hover:decoration-orange-400 hover:font-bold transition-all duration-300"
+            >
+              Our gifts
+            </a>
+            <a
+              href="#"
+              className="hover:underline hover:decoration-orange-400 hover:font-bold transition-all duration-300"
+            >
+              Shop
+            </a>
+            <a
+              href="#"
+              className="hover:underline hover:decoration-orange-400 hover:font-bold transition-all duration-300"
+            >
+              Contact Us
+            </a>
+          </div>
+        )}
+      </AnimatePresence>
+    </>
+  );
+};
 
 export default Nav;
